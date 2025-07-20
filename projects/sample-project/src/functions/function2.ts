fdee6301c2c3af89e8f741822f32d48e7a6338bb
shota-nagato <shota.nagato@gmail.com>
2025-07-20 11:37:37 +0900

    importが冗長になるため、エイリアスを追加

--- projects/sample-project/src/functions/function2.ts
+++ projects/sample-project/src/functions/function2.ts
@@ -1,4 +1,4 @@
-import { Person } from '../../../../shared/clients/class'
+import { Person } from '@/shared/clients/class'
 
 export function function2(): void {
   const person = new Person('John', 30)
